5b1bd0c20271c30fb778c137ab39cab4529d0e0a
Evgeniy1126 <s1mon.ev@yandex.by>
2024-10-15 20:24:52 +0300

    попытка 2.1

--- Attempt 2.py
+++ Attempt 2.py
@@ -1,1 +1,14 @@
-
+12  #int
+1.5  #Float
+str  #str
+True  #bool
+False  #bool
+None  #пустое
+per1 = "Hello"
+per2 = 43
+per3 = 1.7
+per4 = None
+per5 = True
+per6 = 0
+if per2 > per3:
+    print (per6) # 1
