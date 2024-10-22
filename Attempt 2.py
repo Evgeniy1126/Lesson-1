51085f97d058233646aae69d74136d4329cafc16
Evgeniy1126 <s1mon.ev@yandex.by>
2024-10-22 19:41:19 +0300

    Типизация данных (изменения типа данных)

--- Attempt 2.py
+++ Attempt 2.py
@@ -1,14 +1,7 @@
-12  #int
-1.5  #Float
-str  #str
-True  #bool
-False  #bool
-None  #пустое
-per1 = "Hello"
-per2 = 43
-per3 = 1.7
-per4 = None
-per5 = True
-per6 = 0
-if per2 > per3:
-    print (per6) # 1
+per1 = "Helou"
+print(per1)
+per2 = "11"
+print(per2)
+print(type(per2))
+chislo1 = int(per2)
+print (type(chislo1))
